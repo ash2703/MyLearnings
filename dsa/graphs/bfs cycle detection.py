72db578f90a4adb7e9de525bd81f42c6e29f8e72
Aayush Singh <aayushsingh@sharechat.co>
2024-04-18 21:38:58 +0530

    bfs cycle detection undirected

---
+++ dsa/graphs/bfs cycle detection.py
@@ -0,0 +1,88 @@
+"""
+Problem statement
+You have been given an undirected graph with 'N' vertices and 'M' edges. The vertices are labelled from 1 to 'N'.
+
+Your task is to find if the graph contains a cycle or not.
+
+A path that starts from a given vertex and ends at the same vertex traversing the edges only once is called a cycle.
+
+Example :
+
+In the below graph, there exists a cycle between vertex 1, 2 and 3. 
+
+Note:
+
+1. There are no parallel edges between two vertices.
+
+2. There are no self-loops(an edge connecting the vertex to itself) in the graph.
+
+3. The graph can be disconnected.
+For Example :
+
+Input: N = 3 , Edges =  [[1, 2], [2, 3], [1, 3]].
+Output: Yes
+
+Explanation : There are a total of 3 vertices in the graph. There is an edge between vertex 1 and 2, vertex 2 and 3 and vertex 1 and 3. So, there exists a cycle in the graph. 
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
+1 <= T <= 10
+1 <= N <= 5000
+0 <= M <= min(5000, (N * (N - 1)) / 2)
+1 <= edges[i][0] <= N 
+1 <= edges[i][1] <= N 
+
+Time Limit: 1 sec 
+Sample Input 1:
+1
+3 2
+1 2
+2 3
+Sample output 1:
+No
+Explanation of Sample output 1:
+ The above graph can be represented as 
+
+There are a total of 3 vertices in the graph.There is an edge between vertex 1 and 2 and vertex 2 and 3. So, there is no cycle present in the graph. 
+Sample Input 2:
+2
+4 0 
+4 3
+1 4
+4 3
+3 1
+Sample output 2:
+No
+Yes
+"""
+
+from collections import deque
+
+
+def cycleDetection(edges, n, m):
+    adjList = [[] for _ in range(n + 1)]
+    for pair in edges:
+        adjList[pair[0]].append(pair[1])
+        adjList[pair[1]].append(pair[0])
+
+    order = deque()
+    visited = [-1] * (n + 1)
+    for item, value in enumerate(visited):
+        if item == 0:
+            continue
+        if value == -1:
+            order.append((item, -1))
+            visited[item] = 1
+            while order:
+                node, parent = order.popleft()
+                visited[node] = 1
+                for neighbours in adjList[node]:
+                    if visited[neighbours] == -1:
+                        visited[neighbours] = 1
+                        order.append((neighbours, node))
+                    elif parent != neighbours:
+                        return "Yes"
+    return "No"
+
+
+if __name__ == "__main__":
+    print(cycleDetection([[1, 2], [2, 3]], 3, 2))
